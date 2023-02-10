7e62dd68ff3d8f1badb755504e6baae52e024965
Aya0601 <aya61pink@yahoo.co.jp>
2023-02-11 01:58:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ validates :nickname, presence: true, length: { maximum: 6 }
+           :email,presence: true
+           :encrypted_password,presence: true, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
+           :encrypted_password_confirmation,presence: true, confirmation: true
+           :first_name,presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+           :last_name,presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+           :first_name_kana,presence:true, format: { with: /\A[ァ-ヴー]+\z/u }
+           :last_name_kana,presence:true, format: { with:  /\A[ァ-ヴー]+\z/u }
+           :birth,presence: true
 end
