e7d47988faa231d963b26659e96700a0c5af1e62
Aya0601 <aya61pink@yahoo.co.jp>
2023-02-12 00:38:57 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- #validates :nickname, presence: true, length: { maximum: 6 }
- #validates :password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
- #validates :last_name,presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
- #validates :first_name_kana,presence:true, format: { with: /\A[ァ-ヴー]+\z/u }
- #validates :last_name_kana,presence:true, format: { with:  /\A[ァ-ヴー]+\z/u }
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :first_name,presence:true 
+  validates :last_name,presence:true 
+  validates :first_name_kana,presence:true 
+  validates :last_name_kana,presence:true 
   validates :birth,presence: true
 
 end
