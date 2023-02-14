43d11417e378d8e6a741e64fa3d1d43079695c6a
Aya0601 <aya61pink@yahoo.co.jp>
2023-02-14 20:41:04 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name,presence:true 
-  validates :last_name,presence:true 
-  validates :first_name_kana,presence:true 
-  validates :last_name_kana,presence:true 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
   validates :birth,presence: true
 
 end
