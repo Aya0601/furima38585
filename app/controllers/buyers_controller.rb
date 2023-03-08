15314d25ee58be984c5a38dc8b7daf5cfde46291
Aya0601 <aya61pink@yahoo.co.jp>
2023-03-08 22:01:35 +0900

    データベース作成

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,41 @@
+class BuyersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
+  def index
+    @buyer_address = BuyerAddress.new
+  end
+
+  def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      pay_item
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    # この時点では、buyer_idが不要。またrequire外の情報は参照するため、mergeとする。
+    params.require(:buyer_address).permit(:post_code, :area_id, :city, :address_number, :house_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,        # 商品の値段
+      card: buyer_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
+  end
+
+  def non_purchased_item
+    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.buyer.present?
+  end
+end
---
+++ app/models/buyer＿address.rb
@@ -0,0 +1,23 @@
+class BuyerAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address_number, :house_name, :telephone, :token
+
+  with_options presence: true do
+    # buyersのバリデーション
+    validates :user_id
+    validates :item_id
+    # send_addressのバリデーション
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :area_id, numericality: { other_than: 0 }
+    validates :city
+    validates :address_number
+    validates :telephone, format: { with: /\A[0-9]{11}\z/ }
+
+  end
+
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Send_Address.create(buyer_id: buyer.id, post_code: post_code, area_id: area_id, city: city, address_number: address_number, house_name: house_name, telephone: telephone)
+  end
+end
+
