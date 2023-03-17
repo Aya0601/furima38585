e782b910baffca670abd36604f9f44d380661dcd
Aya0601 <aya61pink@yahoo.co.jp>
2023-03-17 22:13:29 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :authenticate_user!
 
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :birth, :first_name, :last_name, :first_name_kana, :last_name_kana]
     )
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '0317'
+    end
   end
 end
